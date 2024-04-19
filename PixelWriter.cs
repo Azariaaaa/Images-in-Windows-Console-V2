3f9b8958beb25516410d962b78ecb22ccad54180
Louis K <71547475+Macbeth57@users.noreply.github.com>
2024-04-19 09:57:37 +0200

    First commit

---
+++ PixelWriter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesInConsole
+{
+    public class PixelWriter
+    {
+        public Bitmap Image { get; set; }
+        public int Accuracy { get; set; }
+
+        public const int SCALING = 3;
+        public int TileWidth { get; set; }
+        public int TileHeight { get; set; }
+
+        public PixelWriter(Bitmap image, int accuracy)
+        {
+            Image = image;
+            Accuracy = accuracy;
+            TileWidth = Image.Width / accuracy;
+            TileHeight = Image.Height / accuracy;
+        }
+
+        public void PictureAnalyzer()
+        {
+            for (int y = 0; y < Image.Height - TileHeight; y += TileHeight)
+            {
+                for (int x = 0; x < Image.Width - TileWidth; x += TileWidth)
+                {
+                    List<string> valuesRGB = GetAverageRGB(x, y);
+                    string R = valuesRGB[0];
+                    string G = valuesRGB[1];
+                    string B = valuesRGB[2];
+
+                    Console.Write("\x1b[38;2;" + R + ";" + G + ";" + B + "m██");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public List<string> GetAverageRGB(int xStart, int yStart)
+        {
+            int redTotal = 0;
+            int greenTotal = 0;
+            int blueTotal = 0;
+            int xEnd = xStart + TileWidth;
+            int yEnd = yStart + TileHeight;
+            int numberOfPixels = TileWidth * TileHeight;
+
+            for (int y = yStart; y < yEnd; y++)
+            {
+                for (int x = xStart; x < xEnd; x++)
+                {
+                    Color pixelColor = Image.GetPixel(x, y);
+                    redTotal += pixelColor.R;
+                    greenTotal += pixelColor.G;
+                    blueTotal += pixelColor.B;
+                }
+            }
+
+            string averageRed = Convert.ToString(redTotal / numberOfPixels);
+            string averageGreen = Convert.ToString(greenTotal / numberOfPixels);
+            string averageBlue = Convert.ToString(blueTotal / numberOfPixels);
+
+            List<string> valuesRGB = new List<string>
+            {
+                averageRed,
+                averageGreen,
+                averageBlue
+            };
+
+            return valuesRGB;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Text.Json.Nodes;
+
+namespace ImagesInConsole
+{
+    internal class Program
+    {
+        public static string url = "https://pokeapi.co/api/v2/pokemon/";
+        static void Main(string[] args)
+        {
+            string pokemonName = AskForAPokemon();
+            string url = GetPokemonImage(pokemonName);
+            Bitmap image = DownloadImageFromUrl(url, pokemonName);
+            //Bitmap picture = new Bitmap("C:\\Users\\krust\\OneDrive\\Bureau\\C#\\ImagesInConsole\\007.png");
+            PixelWriter pw = new PixelWriter(image, 200);
+            pw.PictureAnalyzer();
+        }
+
+        static string AskForAPokemon()
+        {
+            Console.WriteLine("Veuillez entrer le nom d'un pokemon : ");
+            string? pokemonName = Console.ReadLine();
+            return pokemonName;
+        }
+
+        static string GetPokemonImage(string pokemonName)
+        {
+            string fullUrl = url + pokemonName;
+            string json = new WebClient().DownloadString(fullUrl);
+            JsonObject root = JsonNode.Parse(json).AsObject();
+            return (string)root["sprites"]["other"]["official-artwork"]["front_default"];
+        }
+
+        static Bitmap DownloadImageFromUrl(string urlToDownload, string pokemonName)
+        {
+            string url = urlToDownload;
+
+            WebClient client = new WebClient();
+            byte[] imageData = client.DownloadData(url);
+
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream(imageData))
+            {
+                Bitmap bitmap = new Bitmap(ms);
+
+                //bitmap.Save(pokemonName + ".png");
+                //bitmap.Save("C:\\Users\\krust\\OneDrive\\Bureau\\C#\\PokeApiJsonConsole\\" + pokemonName + ".png");
+
+                return bitmap;
+            }
+        }
+    }
+}
